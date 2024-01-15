789bff1d92b8763851466796985d43b52d03e0dc
israelxgbedey <88339733+israelxgbedey@users.noreply.github.com>
2024-01-14 19:22:30 -0600

    Update BankSystem.cpp

--- BankSystem.cpp
+++ BankSystem.cpp
@@ -104,11 +104,11 @@ void calculateBalance(vector<tuple<string, double, string>> transactions) {
     // Check if the user account is valid
     if (user_index != -1) {
         // Display the user account balance and status
-        cout << " Account balance is: " << balances[user_index] << endl;
+        cout << "Account balance is: " << balances[user_index] << endl;
         if (balances[user_index] >= 0) {
-            cout << " Account is in good standing." << endl;
+            cout << "Account is in good standing." << endl;
         } else {
-            cout << " Account is withdrawn." << endl;
+            cout << "Account is withdrawn." << endl;
         }
     } else { // Handle the error if the user account is invalid
         cout << "Error: Invalid account number." << endl;
